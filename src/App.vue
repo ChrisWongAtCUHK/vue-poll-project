f22e47194df64315673db79cf479c2632e2f6408
Chris Wong <wc38.wong@gmail.com>
2023-05-22 14:22:33 +0800

    Reusable Tabs Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
-<script setup>
-  import Header from './components/Header.vue'
-  import Footer from './components/Footer.vue'
+<script>
+import { ref } from 'vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Header: Header,
+    Footer: Footer
+  },
+  props : {
+    
+  },
+  setup() {
+    const itemList = ['Current Polls', 'Add New Poll'];
+    const items = ref(itemList)
+    const activeItem = ref(itemList[0]);
+    return {
+      items, activeItem
+    }
+  },
+
+  mounted() {
+    console.log(this.items)
+    console.log(this.activeItem)
+  }
+}
 </script>
 
 <template>
